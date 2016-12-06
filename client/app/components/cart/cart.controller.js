6016bdae9b6168abf977f5eec3d0e74788c1ea62
Ramses Sotelo Jimenez <sotelo.ramses@gmail.com>
2016-12-06 12:42:52 -0600

    Got Cart functionality partially working

--- client/app/components/cart/cart.controller.js
+++ client/app/components/cart/cart.controller.js
@@ -2,18 +2,10 @@
 
 class CartController {
 	/* @ngInject */
-    constructor($scope, DataService, foobar) {
-		// get store and cart from service
-		console.log("In Cart controller");
+    constructor($scope, DataService) {
 		$scope.store = DataService.store;
 		$scope.cart = DataService.cart;
 		$scope.store.name = "Tom";
-		console.log(foobar);
-
-		// use routing to pick the selected product
-		// if ($routeParams.productId != null) {
-		// 	$scope.product = $scope.store.getProduct($routeParams.productId);
-		// }
 	}
 }
 
